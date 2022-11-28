83778102bd0af6dfdb72db9bb291ff5e73b85497
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-11-28 14:26:29 +0900

    fix(PostController): Use request data as dto object
    Features fixed:
    - Data type title, content: Map<String, String>
    - Better using dto data

--- src/main/java/com/projectchoi/api/controller/PostController.java
+++ src/main/java/com/projectchoi/api/controller/PostController.java
@@ -1,10 +1,8 @@
 package com.projectchoi.api.controller;
 
+import com.projectchoi.api.request.PostCreateDto;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -13,7 +11,7 @@ import java.util.Map;
 public class PostController {
 
     @PostMapping("/posts")
-    public String post(@RequestParam Map<String, String> params) {
+    public String post(@RequestBody PostCreateDto params) {
         log.info("params= {}", params);
         return "Hello its my first blog project";
     }
