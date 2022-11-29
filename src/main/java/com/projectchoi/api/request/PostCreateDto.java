ddfb66cac6bd9de7f239c9555005292fba7eff1e
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-11-29 22:54:56 +0900

    fix(PostCreateDto): Change field access modifier and add NotBlank annotation
    Features fixed:
    - Change fields to private access
    - Add @NotBlank annotation for validation

--- src/main/java/com/projectchoi/api/request/PostCreateDto.java
+++ src/main/java/com/projectchoi/api/request/PostCreateDto.java
@@ -1,12 +1,20 @@
 package com.projectchoi.api.request;
 
+import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.NotBlank;
+
 @ToString
+@Getter
 @Setter
 public class PostCreateDto {
 
-    public String title;
-    public String content;
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String content;
+
 }
