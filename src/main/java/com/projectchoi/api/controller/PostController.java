214161dddf3dc3c2e7262600e4549a6b2fc437c1
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-11-29 22:57:17 +0900

    Add(PostController): add validation and use bindingResult
    Features added:
    - Validate data from post request
    - Use bindingResult for efficient validation logic
    - Change return type to Map

--- src/main/java/com/projectchoi/api/controller/PostController.java
+++ src/main/java/com/projectchoi/api/controller/PostController.java
@@ -2,8 +2,13 @@ package com.projectchoi.api.controller;
 
 import com.projectchoi.api.request.PostCreateDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -11,9 +16,20 @@ import java.util.Map;
 public class PostController {
 
     @PostMapping("/posts")
-    public String post(@RequestBody PostCreateDto params) {
-        log.info("params= {}", params);
-        return "Hello its my first blog project";
+    public Map<String, String> post(@RequestBody @Valid PostCreateDto params, BindingResult result) {
+        if (result.hasErrors()) {
+            List<FieldError> fieldErrors = result.getFieldErrors();
+            FieldError firstFieldError = fieldErrors.get(0);
+
+            String fieldName = firstFieldError.getField();
+            String defaultMessage = firstFieldError.getDefaultMessage();
+
+            Map<String, String> error = new HashMap<>();
+            error.put(fieldName, defaultMessage);
+
+            return error;
+        }
+        return Map.of();
     }
 
 }
