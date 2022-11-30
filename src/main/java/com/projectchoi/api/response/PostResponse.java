c398a576651e91d518d5c5f605b1e59db2d04662
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-11-30 22:15:48 +0900

    add(PostResponse): Overload constructor method
    Features added:
    - Add overloaded constructor and injects Post object fields into PostResponse object

--- src/main/java/com/projectchoi/api/response/PostResponse.java
+++ src/main/java/com/projectchoi/api/response/PostResponse.java
@@ -1,6 +1,7 @@
 package com.projectchoi.api.response;
 
 
+import com.projectchoi.api.domain.Post;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -14,6 +15,13 @@ public class PostResponse {
     private final Long id;
     private final String title;
     private final String content;
+
+    // 생성자 오버로딩
+    public PostResponse(Post post) {
+        this.id = post.getId();
+        this.title = post.getTitle();
+        this.content = post.getContent();
+    }
 
     @Builder
     public PostResponse(Long id, String title, String content) {
