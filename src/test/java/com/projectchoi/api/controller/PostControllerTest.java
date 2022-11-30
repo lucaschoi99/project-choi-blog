0644ed366a965fe51ad56538c49bc42492150ac4
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-11-30 17:16:43 +0900

    fix(PostControllerTest): fix several test logics
    Several features fixed:
    - Use ObjectMapper to match format of json content
    - Changed to more precise test names
    - Add new test to check if data is saved to a repository properly

--- src/test/java/com/projectchoi/api/controller/PostControllerTest.java
+++ src/test/java/com/projectchoi/api/controller/PostControllerTest.java
@@ -1,52 +1,119 @@
 package com.projectchoi.api.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.projectchoi.api.domain.Post;
+import com.projectchoi.api.repository.PostRepository;
+import com.projectchoi.api.request.PostCreateDto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.http.MediaType.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 class PostControllerTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private PostRepository postRepository;
+
+    @BeforeEach
+    void beforeEach() {
+        postRepository.deleteAll();
+    }
+
     @Test
-    @DisplayName("/posts 요청 시 빈 Map을 반환해야 합니다.")
-    public void test() throws Exception {
+    @DisplayName("/posts 요청 시 id를 반환해야 합니다.")
+    public void POST_normal_response_test() throws Exception {
+        // given
+        PostCreateDto request = PostCreateDto.builder()
+                .title("제목입니다")
+                .content("글내용입니다 하하")
+                .build();
+
+        // Object -> String
+        String json = objectMapper.writeValueAsString(request);
+
         // expected
         mockMvc.perform(post("/posts")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"title\": \"제목입니다\", \"content\": \"글내용입니다 하하\"}")
+                        .contentType(APPLICATION_JSON)
+                        .content(json)
                 )
                 .andExpect(status().isOk())
-                .andExpect(content().string("{}"))
-                .andDo(MockMvcResultHandlers.print());
+                .andExpect(content().string("1"))
+                .andDo(print());
     }
 
     @Test
     @DisplayName("/posts 요청 시 title 필드 값은 필수 입니다.")
-    public void test2() throws Exception {
+    public void POST_null_title_test() throws Exception {
+        // given
+        PostCreateDto request = PostCreateDto.builder()
+                .content("글내용입니다 하하")
+                .build();
+
+        // Object -> String
+        String json = objectMapper.writeValueAsString(request);
+
         // expected
         mockMvc.perform(post("/posts")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"title\": null, \"content\": \"글 내용입니다 하하\"}")
+                        .contentType(APPLICATION_JSON)
+                        .content(json)
                 )
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.code").value("400"))
                 .andExpect(jsonPath("$.message").value("Bad Request!"))
                 .andExpect(jsonPath("$.validation.title").value("must not be blank"))
-                .andDo(MockMvcResultHandlers.print());
+                .andDo(print());
 
+    }
+
+    @Test
+    @DisplayName("/posts 요청 시 DB에 값이 저장돼야 합니다.")
+    public void POST_response_db_save_test() throws Exception {
+        // given
+        PostCreateDto request = PostCreateDto.builder()
+                .title("제목입니다")
+                .content("글 내용입니다 하하")
+                .build();
+
+        // Object -> String
+        String json = objectMapper.writeValueAsString(request);
+
+        // expected
+        mockMvc.perform(post("/posts")
+                        .contentType(APPLICATION_JSON)
+                        .content(json)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        // then
+        assertEquals(1L, postRepository.count());
+        Post post = postRepository.findAll().get(0);
+        assertEquals("제목입니다", post.getTitle());
+        assertEquals("글 내용입니다 하하", post.getContent());
     }
 
 }
