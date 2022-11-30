8e4acb73f6d6bbdbdb9d8a416dcdacc3f01e29a3
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-11-30 18:29:59 +0900

    test(PostServiceTest): new test of postService get_single_post_test
    Features to test:
    - Given new post and saved
    - Checks if the post fetched by getSinglePost is equal to the previous post

--- src/test/java/com/projectchoi/api/service/PostServiceTest.java
+++ src/test/java/com/projectchoi/api/service/PostServiceTest.java
@@ -4,11 +4,13 @@ import com.projectchoi.api.domain.Post;
 import com.projectchoi.api.repository.PostRepository;
 import com.projectchoi.api.request.PostCreateDto;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -19,6 +21,11 @@ class PostServiceTest {
 
     @Autowired
     private PostRepository postRepository;
+
+    @BeforeEach
+    void beforeEach() {
+        postRepository.deleteAll();
+    }
 
     @Test
     @DisplayName("글 작성")
@@ -38,4 +45,28 @@ class PostServiceTest {
         assertEquals("글제목", post.getTitle());
         assertEquals("글내용", post.getContent());
     }
+
+    @Test
+    @DisplayName("글 단건조회")
+    void get_single_post_test() {
+        // given
+        Post requestPost = Post.builder()
+                .title("title")
+                .content("content")
+                .build();
+        postRepository.save(requestPost);
+
+        // when
+        Post getPost = postService.getSinglePost(requestPost.getId());
+
+        // then
+        assertNotNull(getPost);
+        assertThat(getPost.getId()).isEqualTo(requestPost.getId());
+        assertEquals("title", getPost.getTitle());
+        assertEquals("content", getPost.getContent());
+    }
+
+
+
+
 }
