b7e5614e81b803bc467c9f64595a62aa08fd1d40
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-11-30 17:04:48 +0900

    feat(Post): Add Entity class for post data
    Features added:
    - Generate id
    - Includes title, content
    - Create constructor and use builder

---
+++ src/main/java/com/projectchoi/api/domain/Post.java
@@ -0,0 +1,29 @@
+package com.projectchoi.api.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Lob // 자바에서는 String, DB 에서는 Long text 로 넘어가도록
+    private String content;
+
+    @Builder
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
