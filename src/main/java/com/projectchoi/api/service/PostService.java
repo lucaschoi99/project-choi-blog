63d8034ebdf1031864047196e10389fd666f80b7
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-11-30 21:03:23 +0900

    fix(PostService): change return type with newly created class PostResponse
    Features fixed:
    - Return with the new class(PostResponse) that satisfies the service policy

--- src/main/java/com/projectchoi/api/service/PostService.java
+++ src/main/java/com/projectchoi/api/service/PostService.java
@@ -3,6 +3,7 @@ package com.projectchoi.api.service;
 import com.projectchoi.api.domain.Post;
 import com.projectchoi.api.repository.PostRepository;
 import com.projectchoi.api.request.PostCreateDto;
+import com.projectchoi.api.response.PostResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -24,8 +25,14 @@ public class PostService {
     }
 
     // 게시글 단건 조회
-    public Post getSinglePost(Long id) {
-        return postRepository.findById(id)
+    public PostResponse getSinglePost(Long id) {
+        Post post = postRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 글입니다."));
+
+        return PostResponse.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .build();
     }
 }
