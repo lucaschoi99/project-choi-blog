9c79f43985371ecfed50df00f5390e331d569b62
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-12-01 17:10:19 +0900

    fix(PostRepository): fix PostRepository extends both JpaRepository and PostRepositoryCustom interface
    Features fixed:
    - So that PostRepository can use both JpaRepository and PostRepositoryCustom interface methods

--- src/main/java/com/projectchoi/api/repository/PostRepository.java
+++ src/main/java/com/projectchoi/api/repository/PostRepository.java
@@ -3,6 +3,5 @@ package com.projectchoi.api.repository;
 import com.projectchoi.api.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
-
+public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
 }
