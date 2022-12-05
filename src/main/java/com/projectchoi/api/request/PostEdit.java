a4694a043fad1d44dd239e9d0247e5a2ea218109
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-12-05 15:10:13 +0900

    feat(PostEdit): add new class dto for edit
    Features created:
    - Dto class for editing post

---
+++ src/main/java/com/projectchoi/api/request/PostEdit.java
@@ -0,0 +1,24 @@
+package com.projectchoi.api.request;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Builder
+public class PostEdit {
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String content;
+
+    @Builder
+    public PostEdit(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+}
