a47d3d420810b09c06661ab733c0573485b2b310
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-01-14 00:00:21 +0900

    fix: 헤더의 cookie 필드에 세션을 넘기는 방식으로 수정
    - 넘어온 accessToken 값을 cookie의 "SESSION" 필드에 넣는 방식으로 수정.

--- src/main/java/com/projectchoi/api/controller/AuthController.java
+++ src/main/java/com/projectchoi/api/controller/AuthController.java
@@ -2,13 +2,17 @@ package com.projectchoi.api.controller;
 
 
 import com.projectchoi.api.request.Login;
-import com.projectchoi.api.response.SessionResponse;
 import com.projectchoi.api.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
 
 @Slf4j
 @RestController
@@ -18,8 +22,19 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/auth/login")
-    public SessionResponse login(@RequestBody Login login) {
+    public ResponseEntity login(@RequestBody Login login) {
         String accessToken = authService.signIn(login);
-        return new SessionResponse(accessToken);
+        ResponseCookie cookie = ResponseCookie.from("SESSION", accessToken)
+                .domain("localhost") // todo 서버 환경에 따른 분리 필요
+                .path("/")
+                .httpOnly(true)
+                .secure(false)
+                .maxAge(Duration.ofDays(30))
+                .sameSite("Strict")
+                .build();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .build();
     }
 }
