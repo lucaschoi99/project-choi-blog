20ae5bff787f7f8c1f75aa0659f1bc1b767aac9d
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-01-13 21:09:41 +0900

    add: Login builder 생성
    - Login 생성자 builder 메서드 추가.

--- src/main/java/com/projectchoi/api/request/Login.java
+++ src/main/java/com/projectchoi/api/request/Login.java
@@ -1,5 +1,6 @@
 package com.projectchoi.api.request;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -17,4 +18,9 @@ public class Login {
     @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 
+    @Builder
+    public Login(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 }
