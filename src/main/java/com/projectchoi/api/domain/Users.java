e0bcad5c0ea6fad6eb60ec1baa00dd2fb0f2e55a
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-01-13 21:11:50 +0900

    add: Session Entity 와의 연관관계 매핑
    - Users - Session 엔티티간의 연관관계 매핑 (Users(One)-Session(Many))

--- src/main/java/com/projectchoi/api/domain/Users.java
+++ src/main/java/com/projectchoi/api/domain/Users.java
@@ -1,14 +1,18 @@
 package com.projectchoi.api.domain;
 
 import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import static javax.persistence.CascadeType.ALL;
+
+@Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Users {
@@ -25,5 +29,23 @@ public class Users {
 
     private LocalDateTime createdAt;
 
+    @OneToMany(cascade = ALL, mappedBy = "users")
+    private List<Session> sessions = new ArrayList<>();
 
+    @Builder
+    public Users(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Session addSession() {
+        Session session = Session.builder()
+                .users(this)
+                .build();
+
+        sessions.add(session);
+        return session;
+    }
 }
