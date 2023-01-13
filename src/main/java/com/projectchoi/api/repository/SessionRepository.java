877b1ffaba0cd7a621646809a8c98a110469d7c0
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-01-14 00:01:08 +0900

    add: SessionRepository 기능 추가.
    - accessToken을 이용한 조회 기능 추가.

--- src/main/java/com/projectchoi/api/repository/SessionRepository.java
+++ src/main/java/com/projectchoi/api/repository/SessionRepository.java
@@ -3,6 +3,10 @@ package com.projectchoi.api.repository;
 import com.projectchoi.api.domain.Session;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface SessionRepository extends CrudRepository<Session, Long> {
 
+    Optional<Session> findByAccessToken(String accessToken);
 }
+
