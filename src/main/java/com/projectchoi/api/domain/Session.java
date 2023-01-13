ce94a48fa262232a26e76426eeac82a14cd90dc9
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-01-13 21:10:20 +0900

    feat: Session Entity 생성
    - 로그인 시 User에게 발급되는 Session 객체.

---
+++ src/main/java/com/projectchoi/api/domain/Session.java
@@ -0,0 +1,36 @@
+package com.projectchoi.api.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import java.util.UUID;
+
+import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = PROTECTED)
+public class Session {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    private String accessToken;
+
+    @ManyToOne
+    private Users users;
+
+    @Builder
+    public Session(Users users) {
+        this.accessToken = UUID.randomUUID().toString();
+        this.users = users;
+    }
+}
