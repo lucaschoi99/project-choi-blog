3705f677ad56e9f55da9e916a58403ea43cef23e
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-01-13 21:10:49 +0900

    feat: Session Entity 레포지토리 생성
    - Session 객체 저장소.

---
+++ src/main/java/com/projectchoi/api/repository/SessionRepository.java
@@ -0,0 +1,8 @@
+package com.projectchoi.api.repository;
+
+import com.projectchoi.api.domain.Session;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SessionRepository extends CrudRepository<Session, Long> {
+
+}
