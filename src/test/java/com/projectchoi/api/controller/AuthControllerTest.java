ea329bfb4394fb2103f028ddda9b82ae087ef130
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-01-13 21:16:46 +0900

    test: AuthController 로그인, 세션 생성과 응답에 대한 테스트
    - 로그인 성공 테스트.
    - 로그인 성공 시 세션 1개가 생성되는지 테스트.
    - 로그인 성공 시 세션 응답이 제대로 되는지 테스트.

---
+++ src/test/java/com/projectchoi/api/controller/AuthControllerTest.java
@@ -0,0 +1,136 @@
+package com.projectchoi.api.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.projectchoi.api.domain.Users;
+import com.projectchoi.api.repository.SessionRepository;
+import com.projectchoi.api.repository.UserRepository;
+import com.projectchoi.api.request.Login;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.transaction.Transactional;
+
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AuthControllerTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private SessionRepository sessionRepository;
+
+    @BeforeEach
+    void beforeEach() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("로그인 성공")
+    public void login_test() throws Exception {
+        // given
+        userRepository.save(Users.builder()
+                .name("ms-choi")
+                .email("lucaschoi@gmail.com")
+                .password("asdf")
+                .build());
+
+        Login login = Login.builder()
+                .email("lucaschoi@gmail.com")
+                .password("asdf")
+                .build();
+
+        // Object -> String
+        String json = objectMapper.writeValueAsString(login);
+
+        // expected
+        mockMvc.perform(post("/auth/login")
+                        .contentType(APPLICATION_JSON)
+                        .content(json)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("로그인 성공 시 세션 1개 생성")
+    public void login_session_test() throws Exception {
+        // given
+        Users usr = userRepository.save(Users.builder()
+                .name("ms-choi")
+                .email("lucaschoi@gmail.com")
+                .password("asdf")
+                .build());
+
+        Login login = Login.builder()
+                .email("lucaschoi@gmail.com")
+                .password("asdf")
+                .build();
+
+        // Object -> String
+        String json = objectMapper.writeValueAsString(login);
+
+        // expected
+        mockMvc.perform(post("/auth/login")
+                        .contentType(APPLICATION_JSON)
+                        .content(json)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        // 세션 생성 확인
+        assertEquals(1L, usr.getSessions().size());
+
+    }
+
+    @Test
+    @DisplayName("로그인 성공 후 세션 응답")
+    public void login_session_response_test() throws Exception {
+        // given
+        Users usr = userRepository.save(Users.builder()
+                .name("ms-choi")
+                .email("lucaschoi@gmail.com")
+                .password("asdf")
+                .build());
+
+        Login login = Login.builder()
+                .email("lucaschoi@gmail.com")
+                .password("asdf")
+                .build();
+
+        // Object -> String
+        String json = objectMapper.writeValueAsString(login);
+
+        // expected
+        mockMvc.perform(post("/auth/login")
+                        .contentType(APPLICATION_JSON)
+                        .content(json)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accessToken", notNullValue()))
+                .andDo(print());
+    }
+
+
+
+}
