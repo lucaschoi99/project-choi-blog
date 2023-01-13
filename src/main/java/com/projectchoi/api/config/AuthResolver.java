a452a463f052eb8298c3b27dff19134a6db9f02f
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-01-14 00:03:53 +0900

    fix: Cookie의 값을 받아 세션 검증하는 방법으로 수정
    - 기존의 헤더에서 "Authorization" 필드의 값을 가져오는 방식에서 Cookie 값에서 accessToken을 가져오는 방법으로 수정.
    - DB에서 세션으로 사용자 확인하여 사용자 id를 반환.

--- src/main/java/com/projectchoi/api/config/AuthResolver.java
+++ src/main/java/com/projectchoi/api/config/AuthResolver.java
@@ -1,14 +1,26 @@
 package com.projectchoi.api.config;
 
 import com.projectchoi.api.config.data.UserSession;
+import com.projectchoi.api.domain.Session;
 import com.projectchoi.api.exception.UnauthorizedUser;
+import com.projectchoi.api.repository.SessionRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+@RequiredArgsConstructor
+@Slf4j
 public class AuthResolver implements HandlerMethodArgumentResolver {
+
+    private final SessionRepository sessionRepository;
+
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
         return parameter.getParameterType().equals(UserSession.class);
@@ -16,14 +28,23 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        String accessToken = webRequest.getHeader("Authorization");
-        if (accessToken == null || accessToken.equals("")) {
+        HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
+        if (servletRequest == null) {
+            log.error("servlet request NULL");
+            throw new UnauthorizedUser();
+        }
+        Cookie[] cookies = servletRequest.getCookies();
+        if (cookies.length == 0) {
+            log.error("No cookie");
             throw new UnauthorizedUser();
         }
 
-        // 데이터베이스 사용자 확인 작업
-        // ...
+        String accessToken = cookies[0].getValue();
 
-        return new UserSession(1L);
+        // 데이터베이스 사용자 확인 작업
+        Session session = sessionRepository.findByAccessToken(accessToken)
+                .orElseThrow(UnauthorizedUser::new);
+
+        return new UserSession(session.getUsers().getId());
     }
 }
