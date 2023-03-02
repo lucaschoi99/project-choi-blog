0ded005314bb600b029a5511f1a2a6179155c559
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-03-02 13:27:09 +0900

    form: Interceptor 방식 쓰지 않음을 comment
    Comment added:
    - 이 프로젝트에서는 Interceptor 방식을 이용하지 않고
    - 인증을 ArgumentResolver로 할 것임을 표시.

--- src/main/java/com/projectchoi/api/config/WebMvcConfig.java
+++ src/main/java/com/projectchoi/api/config/WebMvcConfig.java
@@ -14,14 +14,20 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     private final SessionRepository sessionRepository;
 
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new AuthResolver(sessionRepository));
+    }
+
+    /*
+     * Interceptor 방식
+     * 이 프로젝트에서는 위의 ArgumentResolver 로 인증을 할 것임.
+     */
 //    @Override
 //    public void addInterceptors(InterceptorRegistry registry) {
 //        registry.addInterceptor(new AuthInterceptor())
 //                .excludePathPatterns("/error", "/favicon.ico");
 //    }
 
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new AuthResolver(sessionRepository));
-    }
+
 }
