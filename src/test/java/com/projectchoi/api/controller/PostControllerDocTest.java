f5082c06ebe0597cbe2e30106da3e383aeac4a72
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-03-08 18:06:45 +0900

    test: CRUD 기능 테스트로 api docs 문서화
    Features added:
    - 글 목록 조회, 글 수정, 글 삭제 메서드 추가.
    - API docs 에서 CRUD 요청, 응답 명세 등록됨.

--- src/test/java/com/projectchoi/api/controller/PostControllerDocTest.java
+++ src/test/java/com/projectchoi/api/controller/PostControllerDocTest.java
@@ -1,9 +1,15 @@
 package com.projectchoi.api.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.projectchoi.api.crypt.PasswordEncoder;
 import com.projectchoi.api.domain.Post;
+import com.projectchoi.api.domain.Users;
 import com.projectchoi.api.repository.PostRepository;
+import com.projectchoi.api.repository.UserRepository;
+import com.projectchoi.api.request.Login;
 import com.projectchoi.api.request.PostCreate;
+import com.projectchoi.api.request.PostEdit;
+import com.projectchoi.api.service.AuthService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,13 +21,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import javax.servlet.http.Cookie;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -41,9 +50,19 @@ public class PostControllerDocTest {
     @Autowired
     private PostRepository postRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private AuthService authService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @BeforeEach
     void beforeEach() {
         postRepository.deleteAll();
+        userRepository.deleteAll();
     }
 
     @Test
@@ -51,7 +70,7 @@ public class PostControllerDocTest {
     void post_create() throws Exception {
         // given
         PostCreate request = PostCreate.builder()
-                .title("새로운 글")
+                .title("새로운 제목")
                 .content("새로운 내용")
                 .authorId("1")
                 .build();
@@ -84,19 +103,19 @@ public class PostControllerDocTest {
     void post_inquiry() throws Exception {
         // given
         Post post = Post.builder()
-                .title("Sample_title")
-                .content("Sample_content")
+                .title("새로운 제목")
+                .content("새로운 내용")
                 .authorId("1")
                 .build();
         postRepository.save(post);
 
         // expected
-        this.mockMvc.perform(get("/posts/{postId}", 1L)
+        this.mockMvc.perform(get("/posts/{postId}", post.getId())
                         .accept(APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andDo(document("post-inquiry", pathParameters(
-                                parameterWithName("postId").description("조회글 ID")
+                                parameterWithName("postId").description("조회할 글의 ID")
                         ),
                         responseFields(
                                 fieldWithPath("id").description("글 ID"),
@@ -107,6 +126,142 @@ public class PostControllerDocTest {
                 ));
     }
 
+    @Test
+    @DisplayName("글 여러개 조회")
+    void post_getList_inquiry() throws Exception {
+        // given
+        List<Post> posts = IntStream.rangeClosed(1, 30)
+                .mapToObj(i ->
+                        Post.builder()
+                                .title("새로운 제목" + i)
+                                .content("새로운 내용" + i)
+                                .authorId("1")
+                                .build())
+                .collect(Collectors.toList());
+        postRepository.saveAll(posts);
 
+        // expected
+        this.mockMvc.perform(get("/posts?page=1&size=10")
+                        .accept(APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("post-list-inquiry",
+                        requestParameters(
+                                parameterWithName("page").description("조회할 페이지 번호"),
+                                parameterWithName("size").description("페이지당 글 개수")
+                        ),
+                        responseFields(
+                                fieldWithPath("[].id").description("글 ID"),
+                                fieldWithPath("[].title").description("글 제목"),
+                                fieldWithPath("[].content").description("글 내용"),
+                                fieldWithPath("[].authorId").description("작성자")
+                        )
+                ));
+    }
+
+
+    @Test
+    @DisplayName("글 수정")
+    void post_edit() throws Exception {
+        // given
+        // 유저 등록후 로그인
+        Users user = userRepository.save(Users.builder()
+                .name("minsoo choi")
+                .email("lucaschoi@gmail.com")
+                .password(passwordEncoder.encrypt("asdf"))
+                .build());
+
+        Login login = Login.builder()
+                .email("lucaschoi@gmail.com")
+                .password("asdf")
+                .build();
+
+        // 유저 세션 값 -> Cookie 전달
+        String accessToken = authService.signIn(login);
+        Cookie cookie = new Cookie("SESSION", accessToken);
+
+        // 글 등록 후 수정 요청
+        Post post = Post.builder()
+                .title("새로운 제목")
+                .content("새로운 내용")
+                .authorId(String.valueOf(user.getId()))
+                .build();
+        postRepository.save(post);
+
+        PostEdit postEdit = PostEdit.builder()
+                .title("수정 제목")
+                .content("수정 내용")
+                .build();
+
+        // expected
+        this.mockMvc.perform(patch("/posts/{postId}", post.getId())
+                        .contentType(APPLICATION_JSON)
+                        .accept(APPLICATION_JSON)
+                        .cookie(cookie)
+                        .content(objectMapper.writeValueAsString(postEdit))
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("post-edit",
+                        pathParameters(
+                                parameterWithName("postId").description("수정할 글의 ID")
+                        ),
+                        requestFields(
+                                fieldWithPath("title").description("수정 제목")
+                                        .attributes(key("required").value("Y")),
+                                fieldWithPath("content").description("수정 내용")
+                                        .attributes(key("required").value("Y"))
+                        ),
+                        responseFields(
+                                fieldWithPath("id").description("수정된 글ID"),
+                                fieldWithPath("title").description("수정된 제목"),
+                                fieldWithPath("content").description("수정된 내용"),
+                                fieldWithPath("authorId").description("수정된 글작성자")
+                        )
+                ));
+    }
+
+    @Test
+    @DisplayName("글 삭제")
+    void post_delete() throws Exception {
+        // given
+        // 유저 등록후 로그인
+        Users user = userRepository.save(Users.builder()
+                .name("minsoo choi")
+                .email("lucaschoi@gmail.com")
+                .password(passwordEncoder.encrypt("asdf"))
+                .build());
+
+        Login login = Login.builder()
+                .email("lucaschoi@gmail.com")
+                .password("asdf")
+                .build();
+
+        // 유저 세션 값 -> Cookie 전달
+        String accessToken = authService.signIn(login);
+        Cookie cookie = new Cookie("SESSION", accessToken);
+
+        // 글 등록 후 삭제 요청
+        Post post = Post.builder()
+                .title("새로운 제목")
+                .content("새로운 내용")
+                .authorId(String.valueOf(user.getId()))
+                .build();
+        postRepository.save(post);
+
+        // expected
+        this.mockMvc.perform(delete("/posts/{postId}", post.getId())
+                        .contentType(APPLICATION_JSON)
+                        .accept(APPLICATION_JSON)
+                        .cookie(cookie)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("post-delete",
+                        pathParameters(
+                                parameterWithName("postId").description("삭제할 글의 ID")
+                        )
+                ));
+    }
 
 }
