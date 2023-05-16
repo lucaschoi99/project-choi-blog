2cdd5f96a140852b4fa0db4e0edb8daca0aeda9c
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-05-16 17:25:26 +0900

    fix: Spring Security 도입 후 로그인 테스트 위한 로직 수정

--- src/main/java/com/projectchoi/api/controller/AuthController.java
+++ src/main/java/com/projectchoi/api/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.projectchoi.api.request.SignUp;
 import com.projectchoi.api.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+
+    @GetMapping("/auth/login")
+    public String login() {
+        return "로그인 페이지 입니다.";
+    }
 
     @PostMapping("/auth/signUp")
     public void signUp(@RequestBody SignUp signUp) {
